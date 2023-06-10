db954b77311eb207390ff1a382e4a1c0818d1f6c
luxass <lucasnrgaard@gmail.com>
2023-06-10 19:41:52 +0200

    fix: update blog design

--- app/og/blog/route.tsx
+++ app/og/blog/route.tsx
@@ -4,26 +4,20 @@ export function GET(req: Request) {
   if (!req.url) return new Response("say whaaat", { status: 500 });
   const { searchParams } = new URL(req.url);
   const title = searchParams.get("title") || "No title";
+  const description = searchParams.get("description") || "No description";
   const date = searchParams.get("date") || "No date";
-  const image = searchParams.get("image");
+
 
   return new ImageResponse(
     (
-      <div tw="p-8 bg-stone-900 flex w-full h-full">
+      <div tw="p-8 bg-stone-900 text-white flex w-full h-full">
         <div tw="flex flex-col flex-1">
           <p tw="text-xl">{date}</p>
           <h1 tw="text-7xl">{title}</h1>
+          <p tw="text-2xl">{description}</p>
         </div>
-        <div tw="border-4 border-black rounded-lg w-86 flex items-center justify-center ml-4">
-          {image ?
-              (
-                <p>Hey!</p>
-              ) :
-              (
-               <>
-                 <img src="https://raw.githubusercontent.com/luxass/luxass/main/assets/dino.png" width="320" height="320" />
-               </>
-              )}
+        <div tw="w-86 flex items-end justify-end ml-4">
+          <img src="https://raw.githubusercontent.com/luxass/luxass/main/assets/dino.png" width="256" height="256" />
         </div>
       </div>
     ),
