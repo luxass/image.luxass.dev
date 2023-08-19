e9ba4d6043f19cf432c8fce77bcae1accfa39e0f
luxass <lucasnrgaard@gmail.com>
2023-08-19 15:55:31 +0200

    feat: updated message and added view-source route

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="h-screen w-full flex items-center justify-center bg-black text-white text-xl">
-      <Link href="https://luxass.dev">have a good day</Link>
+      <Link href="https://luxass.dev">have a good day 💙</Link>
     </main>
   );
 }
