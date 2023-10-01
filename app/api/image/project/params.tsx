91123066360380db1e115922c49d897166a86ba2
luxass <lucasnrgaard@gmail.com>
2023-10-01 15:26:52 +0200

    fix: allow for null in project schema

--- app/api/image/project/params.tsx
+++ app/api/image/project/params.tsx
@@ -5,6 +5,6 @@ export const PROJECT_PARAMS = params(
   z.object({
     repo: z
       .custom<`${string}/${string}`>((str) => typeof str === "string" && str.split("/").length === 2),
-    description: z.string().transform((str) => truncateWords(str, 145)).default("No description"),
+    description: z.string().transform((str) => truncateWords(str, 145)).nullable().default("No description"),
   }),
 );
