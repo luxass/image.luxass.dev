e93eb986e5124ab2597806165cc14b181b2d147b
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-11-17 12:38:14 +0100

    chore: lint

--- lib/font.ts
+++ lib/font.ts
@@ -14,8 +14,8 @@ export async function font(family: string, weight?: number, text?: string) {
       text,
     })}`, {
       next: {
-        revalidate: 60 * 60 * 24 * 30 // 30 days
-      }
+        revalidate: 60 * 60 * 24 * 30, // 30 days
+      },
     });
 
   return await res.arrayBuffer();
