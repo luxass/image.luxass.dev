dbddbf98cc027bb39fc3b0935dd405aa4ae58f7b
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-01-07 21:01:54 +0100

    chore(deps): upgrade dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -3,27 +3,8 @@ import {
   luxass,
 } from "@luxass/eslint-config";
 
-import pluginTailwindCSS from "eslint-plugin-tailwindcss";
-
 export default luxass({
   nextjs: true,
   formatters: true,
-}, {
-  plugins: {
-    tailwindcss: pluginTailwindCSS,
-  },
-  rules: {
-    "tailwindcss/classnames-order": ["error"],
-    "tailwindcss/enforces-negative-arbitrary-values": ["warn"],
-    "tailwindcss/enforces-shorthand": ["warn"],
-    "tailwindcss/migration-from-tailwind-2": ["warn"],
-    "tailwindcss/no-arbitrary-value": ["off"],
-    "tailwindcss/no-contradicting-classname": ["error"],
-    "tailwindcss/no-custom-classname": ["warn"],
-  },
-  settings: {
-    tailwindcss: {
-      classRegex: "^(class(Name)?|tw)$",
-    },
-  },
+  tailwindcss: true
 });
