ff9bee815c4653dff08367bb7d4dafdd3b5093ce
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-06-16 12:18:26 +0200

    chore: remove commented svg generation code

--- src/image-response.ts
+++ src/image-response.ts
@@ -8,22 +8,6 @@ export class ImageResponse extends Response {
   ) {
     super();
 
-    // if (options.format === "svg") {
-    //   return (async () => {
-    //     const svg = await render({ element, options });
-    //     return new Response(svg, {
-    //       headers: {
-    //         "Content-Type": "image/svg+xml",
-    //         "Cache-Control": options.debug
-    //           ? "no-cache, no-store"
-    //           : "public, immutable, no-transform, max-age=31536000",
-    //         ...options.headers,
-    //       },
-    //       status: options.status || 200,
-    //       statusText: options.statusText,
-    //     });
-    //   })() as unknown as ImageResponse;
-    // } else {
     const body = new ReadableStream({
       async start(controller) {
         const buffer = await render({
